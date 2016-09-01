8f48b88d84c4166865d9ae9735397e3e8352ae3c
tohara <tom.haraldseid@gmail.com>
2016-09-01 12:29:55 +0800

    backup

--- cmake/ArduinoToolchain.cmake
+++ cmake/ArduinoToolchain.cmake
@@ -10,8 +10,6 @@ set(CMAKE_SYSTEM_NAME Arduino)
 
 set(CMAKE_C_COMPILER   avr-gcc)
 set(CMAKE_CXX_COMPILER avr-g++)
-#set(ARDUINO_SDK_PATH /home/tom/arduino-1.6.8)
-
 
 # Add current directory to CMake Module path automatically
 if(EXISTS  ${CMAKE_CURRENT_LIST_DIR}/Platform/Arduino.cmake)
@@ -22,7 +20,7 @@ endif()
 #                         System Paths                                        #
 #=============================================================================#
 if(UNIX)
-    include(Platform/UnixPaths)
+    include(Platform/Arduino)
     if(APPLE)
         list(APPEND CMAKE_SYSTEM_PREFIX_PATH ~/Applications
                                              /Applications
@@ -35,58 +33,3 @@ elseif(WIN32)
 endif()
 
 
-#=============================================================================#
-#                         Detect Arduino SDK                                  #
-#=============================================================================#
-if(NOT ARDUINO_SDK_PATH)
-    set(ARDUINO_PATHS)
-
-    foreach(DETECT_VERSION_MAJOR 1)
-        foreach(DETECT_VERSION_MINOR RANGE 6 0)
-            list(APPEND ARDUINO_PATHS arduino-${DETECT_VERSION_MAJOR}.${DETECT_VERSION_MINOR})
-            foreach(DETECT_VERSION_PATCH  RANGE 9 0)
-                list(APPEND ARDUINO_PATHS arduino-${DETECT_VERSION_MAJOR}.${DETECT_VERSION_MINOR}.${DETECT_VERSION_PATCH})
-            endforeach()
-        endforeach()
-    endforeach()
-
-    foreach(VERSION RANGE 23 19)
-        list(APPEND ARDUINO_PATHS arduino-00${VERSION})
-    endforeach()
-
-    if(UNIX)
-        file(GLOB SDK_PATH_HINTS /usr/share/arduino*
-            /opt/local/arduino*
-            /opt/arduino*
-            /usr/local/share/arduino*
-            $ENV{HOME}/arduino*)
-    elseif(WIN32)
-        set(SDK_PATH_HINTS "C:\\Program Files\\Arduino"
-            "C:\\Program Files (x86)\\Arduino"
-            )
-    endif()
-    list(SORT SDK_PATH_HINTS)
-    list(REVERSE SDK_PATH_HINTS)
-endif()
-MESSAGE("Arduino version paths:" ${SDK_PATH_HINTS})
-find_path(ARDUINO_SDK_PATH
-          NAMES lib/version.txt
-          PATH_SUFFIXES share/arduino
-                        Arduino.app/Contents/Java/
-                        Arduino.app/Contents/Resources/Java/
-                        ${ARDUINO_PATHS}
-          HINTS ${SDK_PATH_HINTS}
-          DOC "Arduino SDK path.")
-
-if(ARDUINO_SDK_PATH)
-    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${ARDUINO_SDK_PATH}/hardware/tools/avr)
-    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${ARDUINO_SDK_PATH}/hardware/tools/avr/utils)
-else()
-    message(FATAL_ERROR "Could not find Arduino SDK (set ARDUINO_SDK_PATH)!")
-endif()
-
-set(ARDUINO_CPUMENU)
-if(ARDUINO_CPU)
-    set(ARDUINO_CPUMENU ".menu.cpu.${ARDUINO_CPU}")
-endif(ARDUINO_CPU)
-MESSAGE("Arduino  paths:" ${ARDUINO_SDK_PATH})
